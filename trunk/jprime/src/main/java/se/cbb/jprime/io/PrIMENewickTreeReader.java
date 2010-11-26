3cec1f01d491d589c8b64d04c6dcb349a45f72e8
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2010-11-26 22:19:40 +0000

    Added file input (untested as of yet).

--- trunk/jprime/src/main/java/se/cbb/jprime/io/PrIMENewickTreeReader.java
+++ trunk/jprime/src/main/java/se/cbb/jprime/io/PrIMENewickTreeReader.java
@@ -1,6 +1,10 @@
 package se.cbb.jprime.io;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Similar to <code>NewickTreeReader</code>, but provides methods
@@ -20,13 +24,37 @@ public class PrIMENewickTreeReader {
 	 * @return the trees in a list.
 	 * @throws NewickIOException if sanity checks fail.
 	 */
-	public static ArrayList<PrIMENewickTree> readTrees(String str, boolean doSort, boolean strict) throws NewickIOException {
-		ArrayList<NewickTree> rawTrees = NewickTreeReader.readTrees(str, true);
+	public static List<PrIMENewickTree> readTrees(String str, boolean doSort, boolean strict) throws NewickIOException {
+		List<NewickTree> rawTrees = NewickTreeReader.readTrees(str, true);
 		ArrayList<PrIMENewickTree> trees = new ArrayList<PrIMENewickTree>(rawTrees.size());
 		for (NewickTree t : rawTrees) {
 			trees.add(new PrIMENewickTree(t, strict));
 		}
 		return trees;
+	}
+	
+	/**
+	 * Reads consecutive Newick trees (augmented with PrIME meta info) from an input file.
+	 * Empty input is allowed and renders an empty list. Not optimised for large files.
+	 * @param f the input file.
+	 * @param doSort true to sort the tree according to vertex names.
+	 * @param strict set to true to raise an exception if some standard tests fail;
+	 * alternatively one may conduct specific validation using class PrIMENewickTreeVerifier.
+	 * @return the trees in a list.
+	 * @throws NewickIOException if sanity checks fail.
+	 * @throws IOException.
+	 */
+	public static List<PrIMENewickTree> readTrees(File f, boolean doSort, boolean strict) throws NewickIOException, IOException {
+		byte[] buffer = new byte[(int) f.length()];
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(f);
+			fis.read(buffer);
+		} finally {
+			if (fis != null) try { fis.close(); } catch (IOException ex) {}
+		}
+	    String str = new String(buffer);
+	    return readTrees(str, doSort, strict);
 	}
 	
 	/**
@@ -42,6 +70,32 @@ public class PrIMENewickTreeReader {
 	 */
 	public static PrIMENewickTree readTree(String str, boolean doSort, boolean strict) throws NewickIOException {
 		return new PrIMENewickTree(NewickTreeReader.readTree(str, doSort), strict);
+	}
+	
+	/**
+	 * Reads exactly one Newick tree (augmented with PrIME meta info) from an input file.
+	 * Additional trees following the first tree will be ignored and not cause parse errors.
+	 * If input is just comprised of the tree, a semi-colon at the end is not compulsory.
+	 * Not optimised for large files.
+	 * @param f the input file.
+	 * @param doSort true to sort the tree according to vertex names.
+	 * @param strict set to true to raise an exception if some standard tests fail;
+	 * alternatively one may conduct specific validation using class PrIMENewickTreeVerifier.
+	 * @return the tree.
+	 * @throws NewickIOException if sanity checks fail.
+	 * @throws IOException.
+	 */
+	public static PrIMENewickTree readTree(File f, boolean doSort, boolean strict) throws NewickIOException, IOException {
+		byte[] buffer = new byte[(int) f.length()];
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(f);
+			fis.read(buffer);
+		} finally {
+			if (fis != null) try { fis.close(); } catch (IOException ex) {}
+		}
+	    String str = new String(buffer);
+	    return readTree(str, doSort, strict);
 	}
 	
 }
