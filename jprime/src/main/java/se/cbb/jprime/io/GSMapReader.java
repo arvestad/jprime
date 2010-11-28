893be682318a972d2076cdfba858f381e84d6a99
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2010-11-28 11:36:32 +0000

    Added G-S map reader (untested), etc.

---
+++ jprime/src/main/java/se/cbb/jprime/io/GSMapReader.java
@@ -0,0 +1,41 @@
+package se.cbb.jprime.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import se.cbb.jprime.topology.GSMap;
+
+/**
+ * Reads a tab or space delimited file mapping a guest tree leaf to
+ * a host tree leaf, e.g.
+ * <pre>
+ * G1_1   S1
+ * G1_2   S1
+ * G2_1   S2
+ * G3_1   S3
+ * G3_2   S3
+ * </pre>
+ * 
+ * @author Joel Sjöstrand.
+ */
+public class GSMapReader {
+
+	/**
+	 * Reads a G-S map file and returns a corresponding map object.
+	 * @param f the file.
+	 * @return the map.
+	 * @throws FileNotFoundException if no file was found.
+	 */
+	public static GSMap readGSMap(File f) throws FileNotFoundException {
+		GSMap gs = new GSMap();
+		Scanner sc = new Scanner(f);
+		while (sc.hasNextLine()) {
+			String ln = sc.nextLine().trim();
+			String[] parts = ln.split("[ \t]+");
+			gs.add(parts[0], parts[1]);
+		}
+		return gs;
+	}
+	
+}
---
+++ jprime/src/main/java/se/cbb/jprime/topology/GSMap.java
@@ -0,0 +1,78 @@
+package se.cbb.jprime.topology;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Holds a list of guest tree leaf identifiers and a list of
+ * host tree leaf identifiers, and mappings between them.
+ * 
+ * @author Joel Sjöstrand.
+ */
+public class GSMap {
+	
+	/** Guest-to-host map. */
+	private HashMap<String, String> guestToHostLeafMap;
+	
+	/** Host-to-guest map. */
+	private HashMap<String, Set<String>> hostToGuestLeafMap;
+	
+	/**
+	 * Creates an empty map.
+	 */
+	public GSMap() {
+		this.guestToHostLeafMap = new HashMap<String, String>();
+		this.hostToGuestLeafMap = new HashMap<String, Set<String>>();
+	}
+	
+	/**
+	 * Adds a host-guest leaf pair. The Host leaf may already exist
+	 * in other pair constellations.
+	 * @param hostLeaf the host leaf ID.
+	 * @param guestLeaf the guest leaf ID.
+	 */
+	public void add(String hostLeaf, String guestLeaf) {
+		this.guestToHostLeafMap.put(guestLeaf, hostLeaf);
+		Set<String> s = this.hostToGuestLeafMap.get(hostLeaf);
+		if (s == null) {
+			s = new HashSet<String>();
+			this.hostToGuestLeafMap.put(hostLeaf, s);
+		}
+		s.add(guestLeaf);
+	}
+	
+	/**
+	 * Returns the guest leaf IDs.
+	 * @return the IDs.
+	 */
+	public Set<String> getGuestLeafIDs() {
+		return this.guestToHostLeafMap.keySet();
+	}
+	
+	/**
+	 * Returns the host leaf IDs.
+	 * @return the IDs.
+	 */
+	public Set<String> getHostLeafIDs() {
+		return this.hostToGuestLeafMap.keySet();
+	}
+	
+	/**
+	 * Returns the host leaf ID of a guest leaf.
+	 * @param guestLeafID the guest leaf.
+	 * @return the host leaf.
+	 */
+	public String getHostLeafID(String guestLeafID) {
+		return this.guestToHostLeafMap.get(guestLeafID);
+	}
+	
+	/**
+	 * Returns the guest leaf IDs of a host leaf.
+	 * @param guestLeafID the guest leaf.
+	 * @return the host leaf.
+	 */
+	public Set<String> getGuestLeafIDs(String hostLeafID) {
+		return this.hostToGuestLeafMap.get(hostLeafID);
+	}
+}
