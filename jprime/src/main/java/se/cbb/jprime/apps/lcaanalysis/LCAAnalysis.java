7fd93e721b6569c5228101151fa2d973b3d0101a
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2010-11-29 22:12:20 +0000

    Temporary checkin.

---
+++ jprime/src/main/java/se/cbb/jprime/apps/lcaanalysis/LCAAnalysis.java
@@ -0,0 +1,45 @@
+package se.cbb.jprime.apps.lcaanalysis;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import se.cbb.jprime.io.*;
+import se.cbb.jprime.topology.GSMap;
+
+/**
+ * TBD.
+ * 
+ * @author Joel Sjöstrand
+ */
+public class LCAAnalysis {
+
+	/**
+	 * Starter.
+	 * @param args
+	 * @throws IOException .
+	 * @throws NewickIOException.
+	 */
+	public static void main(String[] args) throws NewickIOException, IOException {
+		if (args.length != 2) {
+			System.err.println("Expecting 2 arguments.");
+			usage();
+		}
+		File sFile = new File(args[0]);
+		File gsFile = new File(args[1]);
+		
+		// Read tree and G-S map.
+		PrIMENewickTree s = PrIMENewickTreeReader.readTree(sFile, true, true);
+		GSMap gs = GSMapReader.readGSMap(gsFile);
+		
+	}
+	
+	/**
+	 * Prints usage.
+	 */
+	public static void usage() {
+		System.out.println(
+				"Extracts LCA information for a guest tree inscribed in a host tree.\n" +
+				"usage:    LCAAnalysis <host tree> <GS map>");
+	}
+}
