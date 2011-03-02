cc0fb024ec22354680fef39b07ba44103fbc84d0
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2011-03-02 08:42:16 +0000

    Added comments about MCMCManager algorithm, etc.

--- jprime/src/main/java/se/cbb/jprime/mcmc/ProposerSelector.java
+++ jprime/src/main/java/se/cbb/jprime/mcmc/ProposerSelector.java
@@ -14,9 +14,10 @@ public interface ProposerSelector {
 	 * Selects a subset of a list of proposers which can then be used
 	 * for actual perturbations. It is up to implementing classes to
 	 * decide whether they e.g. return only a single object or multiple
-	 * objects.
+	 * objects. Furthermore, the returned proposers are guaranteed to
+	 * be acting on disjoint state parameters.
 	 * @param proposers the list of proposers to choose among.
-	 * @return a subset of proposers.
+	 * @return a subset of proposers, no pairs acting on the same state parameter.
 	 */
-	public List<Proposer> selectProposers(List<Proposer> proposers);
+	public List<Proposer> selectProposersDisjointly(List<Proposer> proposers);
 }
