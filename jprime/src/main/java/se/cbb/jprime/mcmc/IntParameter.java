9adc727ba92105432b10f0539acde62ba9db9a15
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2011-03-14 19:15:10 +0000

    Added some state parameter stuff, etc.

---
+++ jprime/src/main/java/se/cbb/jprime/mcmc/IntParameter.java
@@ -0,0 +1,110 @@
+package se.cbb.jprime.mcmc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Holds an integer state parameter.
+ * 
+ * @author Joel Sjöstrand.
+ */
+public class IntParameter implements StateParameter {
+
+	/** Name. */
+	private String name;
+	
+	/** Current state. */
+	private int value;
+
+	/** Dependents. */
+	private ArrayList<Dependent> dependents;
+	
+	/** Cache. */
+	private Integer cache;
+	
+	/**
+	 * Constructor.
+	 * @param name parameter's name.
+	 * @param initVal initial value.
+	 */
+	public IntParameter(String name, int initVal) {
+		this.name = name;
+		this.value = initVal;
+		this.dependents = new ArrayList<Dependent>();
+		this.cache = null;
+	}
+	
+	@Override
+	public boolean isSink() {
+		return this.dependents.isEmpty();
+	}
+
+	@Override
+	public void addDependent(Dependent dep) {
+		this.dependents.add(dep);
+	}
+
+	@Override
+	public List<Dependent> getDependents() {
+		return this.dependents;
+	}
+
+	@Override
+	public void cache(boolean willSample) {
+		this.cache = new Integer(this.value);
+	}
+
+	@Override
+	public void update(boolean willSample) {
+	}
+
+	@Override
+	public void clearCache(boolean willSample) {
+		this.cache = null;
+	}
+
+	@Override
+	public void restoreCache(boolean willSample) {
+		this.value = this.cache.intValue();
+	}
+
+	@Override
+	public PerturbationInfo getPerturbationInfo() {
+		return null;
+	}
+
+	@Override
+	public void setPerturbationInfo(PerturbationInfo info) {
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public int getNoOfSubParameters() {
+		return 1;
+	}
+
+	@Override
+	public String getSampleHeader() {
+		return this.name;
+	}
+
+	@Override
+	public String getSampleValue() {
+		return ("" + this.value);
+	}
+
+	@Override
+	public void appendSampleHeader(StringBuilder sb) {
+		sb.append(this.name);
+	}
+
+	@Override
+	public void appendSampleValue(StringBuilder sb) {
+		sb.append(this.value);
+	}
+
+}
--- jprime/src/main/java/se/cbb/jprime/mcmc/StateParameter.java
+++ jprime/src/main/java/se/cbb/jprime/mcmc/StateParameter.java
@@ -12,7 +12,7 @@ package se.cbb.jprime.mcmc;
  *  
  * @author Joel Sjöstrand.
  */
-public interface StateParameter extends Dependent {
+public interface StateParameter extends Dependent, Sampleable {
 
 	/**
 	 * Returns the name of the parameter.
