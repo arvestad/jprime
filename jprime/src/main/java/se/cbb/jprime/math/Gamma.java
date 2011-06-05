7db266c2de0dd3afff712c4ac6bf816bd42945fb
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2011-06-05 10:42:08 +0000

    Started adding gamma distribution.

--- jprime/src/main/java/se/cbb/jprime/math/Gamma.java
+++ jprime/src/main/java/se/cbb/jprime/math/Gamma.java
@@ -37,8 +37,8 @@ public class Gamma {
 	 * Computes the natural logarithm of the complete gamma function,
 	 * i.e., ln(Gamma(xx)) for xx > 0, where
 	 * Gamma(x) = integral(t^(x-1) e^(-t), t=0,...,inf).
-	 * Uses Lanczos' approximation formula.
-	 * See Numerical Recipes 6.1. No bounds checking, see <code>lnGammaSafe(xx)</code>.
+	 * Uses Lanczos' approximation formula, see Numerical Recipes 6.1.
+	 * No bounds checking; see also <code>lnGammaSafe(xx)</code>.
 	 * @param xx input parameter > 0.
 	 * @return ln(Gamma(xx)) where Gamma(.) is the complete gamma function.
 	 */
@@ -67,4 +67,411 @@ public class Gamma {
 		return lnGamma(xx);
 	}
 
+	
+//	//-----------------------------------------------------------------------
+//    //
+//    // Returns percentage points for the gamma distribution, i.e.
+//    // the inverse of the cdf function. Note that notation of beta is such
+//    // that mean=alpha/beta. Sometimes theta=1/beta is used instead.
+//    //
+//    //-----------------------------------------------------------------------
+//  Real ppGamma(const Real& p, const Real& alpha, const Real& beta)
+//  {
+//    assert(alpha > 0.0 && beta > 0.0);// && p > 0.000002);      
+//    return ppchi2(p, 2 * alpha)/(2 * beta);
+//  }
+//  
+//
+//  //------------------------------------------------------------------
+//    //
+//    //! Algorithm As 91,
+//    //! Reference: Best & Roberts, 1975, Appl. Statist 24(3):385-388
+//    //!
+//    //! To evaluate the percentage points of the chi-squared
+//    //! probability distribution function, i.e. it is the inverse of
+//    //! chi-squared's cdf function.
+//    //! P must lie in the range 0.000002 to 0.999998, V must be positive,
+//    //! G must be supplied and should be equal to ln(gamma(V/2.0))
+//    //!
+//    //! INPUT;   P and V
+//    //!          P; Is the cutoff probability
+//    //!	    V; Degree of freedom   V = 2*alpha
+//    //!
+//    //!  Method; Evaluates the Percentage point of the
+//    //!          chi2 square distribution
+//    //!          with the probability P and V degree of freedom
+//    //!          to obtain the percentage point to the gamma distribution
+//    //!
+//    //!  Output; The Percentage point of chi2 square distribution with
+//    //!         probability P and V degree of freedom.
+//    //
+//    //------------------------------------------------------------------
+//  Real ppchi2(const Real& P, const Real& V)
+//  {
+//    // TODO: Instead of throwing error - correct p? /bens
+//    if (P <= 0 || P >= 1 || V < 0.0 )//(P < 2e-6 || P > 0.999998 || V < 0.0 )
+//      {
+//	ostringstream oss;
+//	oss << "PerPoint_CHI2: Percentage point, P = " 
+//	    << P
+//	    << ", is not in range 0.000002-0.999998, and degrees of freedom, V = "
+//	    << V
+//	    << ", is not > 0 and < the numeric limit of double";
+//	throw AnError(oss.str());
+//      }
+//    Real E      = 0.5e-6;
+//    Real AA     = log(2.0);
+////     Real ppchi2 = -1.0;
+//    Real XX     = 0.5 *V;
+//    Real C      = XX - 1.0;
+//    Real G      = lgamma(XX);//log(Gamma(XX));  
+//  
+//    Real ch;
+//    if(V < (-1.24 *log(P)))   // approximation for small chi-squares
+//      {
+//	ch = std::pow(P * XX * std::exp(G + XX * AA), 1.0 / XX);     
+//	if (ch < E)
+//	  {
+//	    return ch;
+//	  }
+//      }  
+//    else 
+//      {
+//	if(V <= 0.32)   // approximation for v <= 0.32 newton raphson metod
+//	  {
+//	    ch = 0.4;
+//	    Real a = log(1.0 - P);
+//	    Real Q;
+//	    do
+//	      {
+//		Q  = ch;
+//		Real P1 = 1.0 + ch * (4.67 + ch);
+//		Real P2 = ch * (6.73 + ch * (6.66 + ch));
+//		Real T  = -0.5 + (4.67 + 2.0 * ch) 
+//		  / P1 - (6.73 + ch * (13.32 + 3.0 * ch)) / P2;
+//		ch  = ch - (1.0 - std::exp(a + G + 0.5 * ch + C * AA) 
+//			    * P2/ P1) / T;
+//	      }
+//	    while (abs(Q / ch - 1) > 0.01); 
+//	  }
+//	else
+//	  {
+//	    // Call to Algorithm As 70 - note that p have been tested above
+//	    Real X = gauinv(P);
+//	    //starting approximation using Wilson and Hilferty estimate
+//	    Real P1 = 0.222222 / V;
+//	    ch = V * pow((X * sqrt(P1) + 1.0 - P1), 3);
+//
+//	    if (ch > (2.2 * V + 6.0)) // Approximation for P tending to 1
+//	      {
+//		ch = -2.0 * (log(1.0 - P) - C * log(0.5 * ch) + G);
+//	      }
+//	  }
+//      }
+//    //Call to algorithm AS32 and calculation of seven term Taylor serie
+//    Real Q  = ch;
+//    do
+//      {
+//	Q  = ch;
+//	Real P1 = 0.5 * ch;
+//	Real P2 = P - gamma_in(P1, XX);
+//	Real T  = P2 * std::exp(XX * AA + G + P1 - C * log(ch)); 
+//	Real B  = T/ch;
+//	Real A  = 0.5 * T - B * C;
+//	Real S1 = (210+A*(140+A*(105+A*(84+A*(70+60*A)))))/420;
+//	Real S2 = (420+A*(735+A*(966+A*(1141+1278*A))))/2520;
+//	Real S3 = (210+A*(462+A*(707+932*A)))/2520;
+//	Real S4 = (252+A*(672+1182*A)+C*(294+A*(889+1740*A)))/5040;
+//	Real S5 = (84+264*A+C*(175+606*A))/2520;
+//	Real S6 = (120+C*(346+127*C))/5040;
+//	ch = ch+T*
+//	  (1+0.5*T*S1-B*C*(S1-B*(S2-B*(S3-B*(S4-B*(S5-B*S6))))));
+//      }
+//    while (abs(Q/ch -1) > E);  
+//    return(ch);
+//  }
+//
+//
+//
+//  //-------------------------------------------------------------------------
+//  //
+//  // Computes incomplete gamma ratio for positive values of arguments P 
+//  // and alpha. Uses series expansion if alpha > X or X < 1.0, otherwise a 
+//  // continued fraction expansion 
+//  //
+//  // INPUT: X and alpha  
+//  //	  X:     is the percentage point for the function
+//  //	  alpha: the alpha value of gammafunction  
+//  //	  X and alpha must be non-negative real value, alpha must be nonzero          
+//  //
+//  //  Reference: Bhattacharjee, 1970, Algorithm As 32, J.R.Stat.Soc.C. 19(3)
+//  //
+//  //  Output: the value of the incomplete gamma function for X and alpha.
+//  //
+//  //-------------------------------------------------------------------------
+//  Real
+//  gamma_in(const Real& X, const Real& alpha)
+//  {
+//    if (X <= 0 || alpha <= 0)
+//      {
+//	if(X == 0)
+//	  {
+//	    return 0;
+//	  }     
+//	else
+//	  { 
+//	    throw AnError("X and alpha must be non-negative real value");
+//	  }
+//      }
+//  
+//    Real acu     = 1.0e-8;
+//    Real oflo    = 1.0e30;
+//    Real g_in    = 0.0;
+//    Real G       = lgamma(alpha);
+//    Real factor  = std::exp(alpha * log(X) - X - G);
+//
+//    Real xbig = 1.0E6;
+//    Real alimit = 1000.0;
+//
+//    // Use normal approximation if alpha > alimit
+//    if(alpha > alimit)
+//      {
+//	Real pn1 = 3 * std::sqrt(alpha) * (std::pow(X / alpha, 1.0/3.0) + 
+//				      1.0 / (9.0*alpha) - 1.0);
+//	return alnorm(pn1, false);
+//      }
+//    if(X > xbig)
+//      {
+//	return 1.0;
+//      }
+//
+//    // Series expansion for x<alpha+1
+//    if (X > 1.0 && X >= alpha)
+//      {
+//	// Continued fraction for x>= alpha+1
+//	Real a    = 1.0 - alpha;
+//	Real b    = a + X + 1.0;
+//	Real term = 0.0;
+//	Real pn[6];
+//	pn[0]     = 1.0;
+//	pn[1]     = X;
+//	pn[2]     = X + 1.0;
+//	pn[3]     = X * b;
+//	g_in      = pn[2] / pn[3];
+//	Real dif = 1.0;
+//	Real rn = 0.0;
+//	do
+//	  {
+//	    a       += 1.0;
+//	    b       += 2.0;
+//	    term    += 1.0;
+//	    Real an = a * term;
+//	    for(unsigned i = 0; i < 2; i++)
+//	      {	      
+//		pn[i+4] = b * pn[i+2] - an * pn[i];
+//	      }
+//	    if(pn[5] != 0.0)
+//	      {
+//		rn = pn[4] / pn[5];
+//		dif = abs(g_in - rn);
+//		if(dif <= acu)
+//		  {
+//		    if(dif <= acu* rn)
+//		      {
+//			return 1.0 - factor * g_in;
+//		      }
+//		  }
+//		g_in =rn;
+//	      }
+//	    for(unsigned i = 0; i < 4; i++)
+//	      {
+//		pn[i] = pn[i+2];
+//	      }
+//	    if(abs(pn[4]) >= oflo)
+//	      {
+//		for(unsigned i = 0; i < 4; i++)
+//		  {
+//		    pn[i] = pn[i] / oflo;
+//		  }
+//	      }
+//	  }
+//	while(true); 
+//      }
+//    else
+//      {
+//	g_in = 1.0;
+//	Real term = 1.0;
+//	Real rn = alpha;
+//	do
+//	  {
+//	    rn   += 1.0;
+//	    term *= X / rn;
+//	    g_in += term;
+//	  }
+//	while(term > acu);
+//      
+//	return g_in * factor / alpha;
+//      }
+//  }
+//
+//
+//
+//
+//  //-------------------------------------------------------
+//  //
+//  // ACM algorithm 291
+//  // Logarithm of the Gamma function
+//  // Pike & Hill
+//  //
+//  // This procedure evaluates the natural logarithm of
+//  // gamma(x) for all x > 0, accurate to the 10 decimal 
+//  // places. Stirling's formula is used for the central 
+//  // polynomial part of the procedure
+//  //
+//  //-------------------------------------------------------
+//  Real
+//  loggamma_fn(Real x)
+//  {
+//    Real f = 0.0;
+//    Real z;
+//    if(x < 7.0)
+//      {
+//	f = 1.0;
+//	for(z = x; z < 7.0; z += 1.0)
+//	  {
+//	    x = z;
+//	    f *= z;
+//	  }
+//	x += 1.0;
+//	f = - log(f);
+//      }
+//    z = 1.0 / (x * x);
+//    return f + (x -0.5) * log(x) - x + 0.918938533204673 +
+//      (((-0.000595238095238 * z + 0.000793650793651) * z - 
+//	0.002777777777778) * z + 0.083333333333333) / x;
+//  }
+//
+//  //------------------------------------------------------------------
+//  //
+//  // Gauinv finds percentage points of the normal distribution,
+//  // i.e. it is the inverse of the cdf function.
+//  //
+//  // Algorithm As70
+//  // Odeh and Evans, 1974 Applied Statistics 23(1):96-97
+//  //
+//  // Input: Real P value of lower tail area p
+//  // Gauinv finds percentage points of the normal distribution
+//  // Kollad!
+//  //-----------------------------------------------------------
+//  Real
+//  gauinv(const Real& P)
+//  {
+//    Real alimit = 1.0e-20;
+//
+//    Real p0 = -0.322232431088;
+//    Real p1 = -1.0;
+//    Real p2 = -0.342242088547;
+//    Real p3 = -0.204231210245e-1;
+//    Real p4 = -0.453642210148e-4;
+//
+//    Real q0 = 0.993484626060e-1;
+//    Real q1 = 0.588581570495;
+//    Real q2 = 0.531103462366;
+//    Real q3 = 0.103537752850;
+//    Real q4 = 0.38560700634e-2;
+//
+//    Real ps = P;
+//    if(ps > 0.5)
+//      {
+//	ps = 1.0 -ps;
+//      }
+//    if(ps < alimit) 
+//      {
+//	throw AnError("gauinv: P is not in the interval [10e-20, 1-10e-20]");
+//      }
+//    if(ps == 0.5)
+//      {
+//	return 0.0;
+//      }
+//    Real yi =sqrt(log(1.0 / (ps *ps)));
+//    Real gauinv = yi + 
+//      ((((yi * p4 + p3) * yi + p2) * yi + p1) * yi + p0) / 
+//      ((((yi * q4 + q3) * yi + q2) * yi + q1) * yi + q0);
+//    if(P < 0.5)
+//      {
+//	return -gauinv;
+//      }
+//    else
+//      {
+//	return gauinv;
+//      }
+//  }
+//
+//
+//  // Algorithm AS 66 - The normal integral
+//  //
+//  // by I.D. HIll, 1973, Applied Statistics 22(3):424-427
+//  // Calculates the upper or lower tail area of the standard normal 
+//  // distribution curve corresponding to any given argument.
+//  //
+//  // Parameters: Real x - the argument value of the normal distr function
+//  //             bool upper - true = calculate area from x to infinity
+//  //                          false = calclat area from 0 to x
+//  //
+//  // Data constants: LTONE should be set to the value at which the lower
+//  //                 tail area becomes 1.0 to the accuracy of the machine.
+//  //                 LTONE=(n+9)/3 gives the rquired value accurately 
+//  //                 enough for a machine that produces n nedical digits 
+//  //                 in its real number.
+//  //                 UTZERO should be set to the value at which the upper
+//  //                 tail area becomes 0.0 to the accuracy of the machine.
+//  //                 This may be taken as the value such that
+//  //                 exp(-0.5*UTZERO�)/(UTZERO*scrt(2*pi)) is just greater
+//  //                 than the smallest allowable real number.
+//  //--------------------------------------------------------------------
+//  Real
+//  alnorm(Real x, bool upper)
+//  {
+//    // This is waht is recommended, but...
+//        Real LTONE = (Real_limits::digits10 +9)/3;
+//        Real UTZERO = 37.4949; // Suits Apple PowerBook G4 w MacOSX
+//    
+//    // ...this seems to work:-o
+////     Real LTONE = Real_limits::max();
+////     Real UTZERO = Real_limits::max();
+//
+//    Real alnorm = 0;
+//
+//    if(x < 0)
+//      {
+//	upper = !upper;
+//	x = -x;
+//      }
+//    if(x <= LTONE || (upper && x <= UTZERO))
+//      {
+//	Real y = 0.5 * x * x;
+//	if(x <= 1.28)
+//	  {
+//	    alnorm = 0.5 - x * (0.398942280444 - 0.399903438504 * y /
+//				(y + 5.75885480458 - 29.8213557808 /
+//				 (y + 2.62433121679 + 48.6959930692 /
+//				  (y + 5.92885724438))));
+//	  }
+//	else
+//	  {
+//	    alnorm = 0.398942280385 * std::exp(-y) /
+//	      (x - 3.8052E-8 + 1.00000615302 /
+//	       (x + 3.98064794E-4 +1.98615381364 /
+//		(x - 0.151679116635 + 5.29330324926 /
+//		 (x + 4.8385912808 -15.1508972451 /
+//		  (x + 0.742380924027 + 30.789933034 /
+//		   (x + 3.99019417011))))));
+//	  }
+//      }
+//    if(upper == false)
+//      {
+//	alnorm = 1.0 - alnorm;
+//      }
+//    return alnorm;
+//  }
 }
---
+++ jprime/src/main/java/se/cbb/jprime/math/GammaDistribution.java
@@ -0,0 +1,345 @@
+package se.cbb.jprime.math;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import se.cbb.jprime.mcmc.ChangeInfo;
+import se.cbb.jprime.mcmc.Dependent;
+import se.cbb.jprime.mcmc.DoubleParameter;
+
+/**
+ * Represents a 1-D gamma distribution Gamma(k,theta).
+ * It is possible to let the distribution rely on two floating point state parameters and
+ * act as a <code>Dependent</code>.
+ * <p/>
+ * Beware of the alternative parametrisation that is sometimes used. We use
+ * <i>shape</i> parameter k and <i>scale</i> parameter theta. Sometimes, one
+ * encounters Gamma(alpha,beta), with shape parameter alpha=k and <i>rate</i> parameter
+ * beta=1/theta.
+ * <p/>
+ * <b>Note: As of yet, many methods which involve extensive numerical computations
+ * have not been implemented.</b>
+ * 
+ * @author Martin Linder.
+ * @author Bengt Sennblad.
+ * @author Joel Sjöstrand.
+ */
+public class GammaDistribution implements Continuous1DPD, Dependent {
+
+	/**
+	 * When the distribution depends on state parameters, defines
+	 * what the latter represent.
+	 */
+	public enum ParameterSetup {
+		/** Shape and scale. */                    K_AND_THETA,
+		/** Mean and standard deviation. */        MEAN_AND_STDEV,
+		/** Mean and coefficient of variation. */  MEAN_AND_CV
+	}
+	
+	/** First state parameter. Null if not used. */
+	protected DoubleParameter p1;
+	
+	/** Second state parameter. Null if not used. */
+	protected DoubleParameter p2;
+	
+	/** State parameter representation. Null if not used. */
+	protected ParameterSetup setup;
+	
+	/** Shape parameter. Should reflect p1 and p2's values. */
+	protected double k;
+	
+	/** Scale parameter. Should reflect p1 and p2's values. */
+	protected double theta;
+	
+	/** For speed, holds -ln(G(k)*theta^k), where G() is the gamma function. */
+	protected double c;
+	
+	/** Child dependents. */
+	protected TreeSet<Dependent> dependents;
+	
+	/** Shape cache. */
+	private double kCache = Double.NaN;
+
+	/** Scale cache. */
+	private double thetaCache = Double.NaN;
+
+	/** C cache. */
+	private double cCache = Double.NaN;
+	
+	/** Change info. */
+	protected ChangeInfo changeInfo = null;
+	
+	/**
+	 * Constructor for when the distribution does not rely on state parameters.
+	 * Note the parametrisation where <i>scale</i> parameter theta=1/beta, for <i>rate</i> parameter beta.
+	 * @param k distribution shape parameter.
+	 * @param theta distribution scale parameter.
+	 */
+	public GammaDistribution(double k, double theta) {
+		if (k <= 0.0 || theta <= 0.0) {
+			throw new IllegalArgumentException("Cannot have non-positive shape or scale for gamma distribution.");
+		}
+		this.p1 = null;
+		this.p2 = null;
+		this.setup = null;
+		this.dependents = null;
+		this.k = k;
+		this.theta = theta;
+		this.update(false);
+	}
+	
+	/**
+	 * Constructor for when the distribution relies on state parameters.
+	 * The distribution will add itself as a child dependent of the parameters.
+	 * @param p1 the first parameter.
+	 * @param p2 the second parameter.
+	 * @param setup what p1 and p2 represents.
+	 */
+	public GammaDistribution(DoubleParameter p1, DoubleParameter p2, ParameterSetup setup) {
+		this.p1 = p1;
+		this.p2 = p2;
+		this.setup = setup;
+		this.dependents = new TreeSet<Dependent>();
+		p1.addChildDependent(this);
+		p2.addChildDependent(this);
+		this.update(false);
+	}
+	
+	@Override
+	public String getName() {
+		return "Gamma distribution";
+	}
+
+	@Override
+	public int getNoOfParameters() {
+		return 2;
+	}
+
+	@Override
+	public int getNoOfDimensions() {
+		return 1;
+	}
+
+	@Override
+	public boolean isDependentSink() {
+		return this.dependents.isEmpty();
+	}
+
+	@Override
+	public void addChildDependent(Dependent dep) {
+		this.dependents.add(dep);
+	}
+
+	@Override
+	public Set<Dependent> getChildDependents() {
+		return this.dependents;
+	}
+
+	@Override
+	public void cache(boolean willSample) {
+		this.kCache = this.k;
+		this.thetaCache = this.theta;
+		this.cCache = this.c;
+	}
+
+	@Override
+	public void update(boolean willSample) {
+		// We always do an update.
+		if (this.p1 != null) {
+			String old = this.toString();
+			switch (this.setup) {
+			case K_AND_THETA:
+				this.k = this.p1.getValue();
+				this.theta = this.p2.getValue();
+				break;
+			case MEAN_AND_STDEV:
+				this.k = Math.pow(this.p1.getValue() / this.p2.getValue(), 2);
+				this.theta = Math.pow(this.p2.getValue(), 2) / this.p1.getValue();
+				break;
+			case MEAN_AND_CV:
+				this.k = 1.0 / Math.pow(this.p1.getValue(), 2);
+				this.theta = this.p1.getValue() * Math.pow(this.p2.getValue(), 2);
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown parameter setup for gamma distribution.");
+			}
+			this.c = -this.k * Math.log(this.theta) - Gamma.lnGamma(this.k);
+			this.changeInfo = new ChangeInfo(this, "Proposed: " + this.toString() + ", Old: " + old);
+		} else {
+			this.c = -this.k * Math.log(this.theta) - Gamma.lnGamma(this.k);
+		}
+	}
+
+	@Override
+	public void clearCache(boolean willSample) {
+		this.kCache = Double.NaN;
+		this.thetaCache = Double.NaN;
+		this.cCache = Double.NaN;
+		this.changeInfo = null;
+	}
+
+	@Override
+	public void restoreCache(boolean willSample) {
+		this.k = this.kCache;
+		this.theta = this.thetaCache;
+		this.c = this.cCache;
+		this.k = Double.NaN;
+		this.thetaCache = Double.NaN;
+		this.cCache = Double.NaN;
+		this.changeInfo = null;
+	}
+
+	@Override
+	public ChangeInfo getChangeInfo() {
+		return this.changeInfo;
+	}
+
+	@Override
+	public double getPDF(double x) {
+		return Math.exp((this.k - 1.0) * Math.log(x) - x / this.theta + this.c);
+	}
+	
+	/**
+	 * For speed reasons, provides access to the probability density function f(x) for
+	 * a specified value x, returned as a <code>Probability</code>.
+	 * @param x the value where to evaluate density.
+	 * @return the probability density, f(x).
+	 */
+	public Probability getPDFAsProbability(double x) {
+		return new Probability((this.k - 1.0) * Math.log(x) - x / this.theta + this.c, 1);
+	}
+
+	@Override
+	public double getCDF(double x) {
+		// TODO: Implement.
+//		if (x < 0) {
+//			return 0.0;
+//		}
+//		double invG = Gamma.gamma_in(x / this.theta, this.k);
+//		if (invG < 0.0) {
+//			return 0.0;
+//		} else if (invG > 1.0) {
+//			return 1.0;
+//		}
+//		return invG;
+		throw new UnsupportedOperationException("Numerical estimate of gamma CDF not implemented yet.");
+	}
+
+	@Override
+	public double getQuantile(double p) {
+		// TODO: Implement.
+		throw new UnsupportedOperationException("Numerical estimate of gamma quantile function not implemented yet.");
+	}
+
+	@Override
+	public double getProbability(double a, double b) {
+		return (this.getCDF(b) - this.getCDF(a));
+	}
+
+	@Override
+	public double getMean() throws MathException {
+		return (this.k * this.theta);
+	}
+
+	/**
+	 * Sets the mean by changing the shape parameter but not scale parameter.
+	 * @param mean the new mean.
+	 */
+	@Override
+	public void setMean(double mean) {
+		if (mean <= 0.0) { throw new IllegalArgumentException("Cannot set non-positive mean on gamma distribution."); }
+		this.k = mean / this.theta;
+		this.c = -this.k * Math.log(this.theta) - Gamma.lnGamma(this.k);
+		if (this.p1 != null) {
+			synchP1AndP2();
+		}
+	}
+
+	@Override
+	public double getMedian() {
+		return this.getQuantile(0.5);
+	}
+
+	@Override
+	public double getStandardDeviation() {
+		return Math.sqrt(this.k) * this.theta;
+	}
+
+	/**
+	 * Sets the standard deviation by changing the shape parameter but not scale parameter.
+	 * @param stdev the new standard deviation.
+	 */
+	@Override
+	public void setStandardDeviation(double stdev) {
+		if (stdev <= 0.0) { throw new IllegalArgumentException("Cannot set non-positive variance on gamma distribution."); }
+		this.k = Math.pow(stdev / this.theta, 2);
+		this.c = -this.k * Math.log(this.theta) - Gamma.lnGamma(this.k);
+		if (this.p1 != null) {
+			synchP1AndP2();
+		}
+	}
+
+	@Override
+	public double getVariance() {
+		return (this.k * this.theta * this.theta);
+	}
+
+	/**
+	 * Sets the variance by changing the shape parameter but not scale parameter.
+	 * @param var the new variance.
+	 */
+	@Override
+	public void setVariance(double var) {
+		if (var <= 0.0) { throw new IllegalArgumentException("Cannot set non-positive variance on gamma distribution."); }
+		this.k = var / (this.theta * this.theta);
+		this.c = -this.k * Math.log(this.theta) - Gamma.lnGamma(this.k);
+		if (this.p1 != null) {
+			synchP1AndP2();
+		}
+	}
+
+	/**
+	 * Adjusts p1 and p2 in accordance with the current internal values
+	 * following a change of the shape parameter.
+	 */
+	private void synchP1AndP2() {
+		switch (this.setup) {
+		case K_AND_THETA:
+			this.p1.setValue(this.k);
+			break;
+		case MEAN_AND_STDEV:
+			this.p1.setValue(this.k * this.theta);
+			this.p2.setValue(Math.sqrt(this.k) * this.theta);
+			break;
+		case MEAN_AND_CV:
+			this.p1.setValue(this.k * this.theta);
+			this.p2.setValue(1.0 / Math.sqrt(this.k));
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown parameter setup for normal distribution.");
+		}
+	}
+	
+	@Override
+	public double getCV() {
+		return (1.0 / Math.sqrt(k));
+	}
+
+	@Override
+	public RealInterval getDomainInterval() {
+		return new RealInterval(0, Double.POSITIVE_INFINITY, false, true);
+	}
+
+	@Override
+	public double sampleValue(PRNG prng) {
+		// TODO: Implement.
+		//return Gamma.ppGamma(p, alpha, beta); 
+		throw new UnsupportedOperationException("Sampling from gamma function not implemented yet.");
+	}
+
+	@Override
+	public String toString() {
+		return "Gamma(" + this.k + ", " + this.theta + ')';
+	}  
+
+}
