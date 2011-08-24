30ceac5fa457567c2ee6bfca80acd9425a197271
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2011-08-24 14:26:42 +0000

    Name change...

--- trunk/jprime/src/test/java/se/cbb/jprime/apps/phylotools/TestFilterInparalogues.java
+++ trunk/jprime/src/test/java/se/cbb/jprime/apps/phylotools/TestFilterInparalogues.java
@@ -17,21 +17,21 @@ import se.cbb.jprime.topology.TopologyException;
  * 
  * @author Joel Sjöstrand.
  */
-public class TestInparalogueFilterer {
+public class TestFilterInparalogues {
 	
 	@Test
-	public void testInparalogueFilterer() throws NewickIOException, IOException, TopologyException {
+	public void testFilterInparalogues() throws NewickIOException, IOException, TopologyException {
 		// Contrived case.
 		PrIMENewickTree SRaw = PrIMENewickTreeReader.readTree("((A,B),C);", false, false);
 		PrIMENewickTree GRaw = PrIMENewickTreeReader.readTree("((a1:0.5,a2:0.6):0.8,(a3:0.2,a4:0.3):0.9):0.3;", false, false);
 		GuestHostMap GS = GuestHostMapReader.readGuestHostMap("a1\tA\na2\tA\na3\tA\na4\tA");
-		String s = InparalogueFilterer.filterInparalogues(GRaw, SRaw, GS);
+		String s = FilterInparalogues.filterInparalogues(GRaw, SRaw, GS);
 		assertEquals("a1:1.6;", s);
 		// More complex case.
 		SRaw = PrIMENewickTreeReader.readTree("(A,(B,C));", false, false);
 		GRaw = PrIMENewickTreeReader.readTree("(((a1:0.5,a2:0.5):0.5,b1:0.5):0.5,((a3:0.5,a4:0.5):0.5,((c1:0.5,c2:0.5):0.5,c3:0.5):0.5):0.5):0.5;", false, false);
 		GS = GuestHostMapReader.readGuestHostMap("a1\tA\na2\tA\na3\tA\na4\tA\nb1\tB\nc1\tC\nc2\tC\nc3\tC");
-		s = InparalogueFilterer.filterInparalogues(GRaw, SRaw, GS);
+		s = FilterInparalogues.filterInparalogues(GRaw, SRaw, GS);
 		assertEquals("((a1:1.0,b1:0.5):0.5,(a3:1.0,c1:1.5):0.5):0.5;", s);
 	}
 	
