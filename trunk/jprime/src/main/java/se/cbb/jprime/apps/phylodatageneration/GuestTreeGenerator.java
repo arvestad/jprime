59db93160d393b70b22d3cd39698a46cbf74d841
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2013-01-22 14:45:05 +0000

    Generated stubs for tree/sequence data generation.

---
+++ trunk/jprime/src/main/java/se/cbb/jprime/apps/phylodatageneration/GuestTreeGenerator.java
@@ -0,0 +1,25 @@
+package se.cbb.jprime.apps.phylodatageneration;
+
+import se.cbb.jprime.apps.JPrIMEApp;
+
+/**
+ * Generates an ultrametric guest tree ("gene tree") involving inside a host tree ("species tree"),
+ * by means of a BD process or similarly.
+ * 
+ * @author Joel Sjöstrand.
+ */
+public class GuestTreeGenerator implements JPrIMEApp {
+
+	@Override
+	public String getAppName() {
+		return "GuestTreeGenerator";
+	}
+
+	@Override
+	public void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
