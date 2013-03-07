78c7dd9353dd7d1c57fc858ff9f681edf4fd466f
joel.sjostrand <joel.sjostrand@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2013-03-07 17:09:35 +0000

    Started adding GML implementation.

---
+++ trunk/jprime/src/main/java/se/cbb/jprime/io/GMLEdge.java
@@ -0,0 +1,17 @@
+package se.cbb.jprime.io;
+
+/**
+ * Represents a GML edge. Example:
+ * <pre>
+ * edge [
+ *   source 1
+ *   target 2
+ *   label "Edge from node 1 to node 2"
+ * ]
+ * </pre>
+ * 
+ * @author Joel Sjöstrand.
+ */
+public class GMLEdge {
+
+}
---
+++ trunk/jprime/src/main/java/se/cbb/jprime/io/GMLNode.java
@@ -0,0 +1,20 @@
+package se.cbb.jprime.io;
+
+/**
+ * Represents a GML node.
+ * Example:
+ * <pre>
+ *  node [
+                id 1
+                label "node 1"
+                thisIsASampleAttribute 42
+        ]
+ * </pre>
+ * 
+ * @author Joel Sjöstrand.
+ */
+public class GMLNode {
+
+	
+	
+}
