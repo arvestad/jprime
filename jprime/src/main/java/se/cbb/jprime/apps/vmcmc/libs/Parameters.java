21944e693af80da0e9b5f04988122c0104e3bdb0
demonhunter@gmail.com <demonhunter@gmail.com@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2013-11-26 14:09:27 +0000

    Updated VMCMC input options and also the JSON parser

--- jprime/src/main/java/se/cbb/jprime/apps/vmcmc/libs/Parameters.java
+++ jprime/src/main/java/se/cbb/jprime/apps/vmcmc/libs/Parameters.java
@@ -13,16 +13,12 @@ import com.beust.jcommander.Parameter;
 public class Parameters {
 
 	/** Required parameters: None */
-	@Parameter(description = "None. VMCMC start window will appear, if no arguments are passed.")
+	@Parameter(description = "None or filename. VMCMC start window will appear, if no arguments are passed and VMCMC will display results and data in GUI if arguments filname is given.")
 	public List<String> files = new ArrayList<String>();
 	
 	/** Help. */
 	@Parameter(names = {"-h", "--help"}, description = "Display help. To understand the options and input parameters, use help.")
 	public Boolean help = false;
-	
-	/** Filename */
-	@Parameter(names = {"-f","--filename"}, description = "Input file. View all the results and data with a Graphical User Interface for the file FILENAME. Check -n for same results on command line.\nDefault: No file")
-	public String filename = null;
 	
 	/** Test and Simple Statistics only */
 	@Parameter(names = {"-n","--nogui"}, description = "Test and simple statistics shown on command line only. VMCMC computed statistics and tests shown on stdout.")
@@ -48,15 +44,23 @@ public class Parameters {
 	@Parameter(names = {"-e","--ess"}, description = "Effective Sample Size test result on command line only. VMCMC computed estimated sample size burn-in and convergenceestimator for the MCMC chain shown on stdout. ")
 	public Boolean ess = false;
 	
-	/** Estimated Sample Size Test only */
+	/** Gelman-Rubin convergence test only */
 	@Parameter(names = {"-r","--gelmanrubin"}, description = "Gelman-Rubin convergence test result on command line only. VMCMC computed gelman rubin burn-in and convergence estimator for the MCMC chain shown on stdout. ")
 	public Boolean gr = false;
 	
-	/** Estimated Sample Size Test only */
-	@Parameter(names = {"-mt","--maptree"}, description = "Display MAP trees. ")
-	public Boolean tree = false;
+	/** Display Posterior distribution of trees only */
+	@Parameter(names = {"-p","--posterior"}, description = "Display Posterior distribution of trees. ")
+	public Boolean posterior = false;
 	
 	/** Estimated Sample Size Test only */
 	@Parameter(names = {"-b","--burnin"}, description = "Burn-in values to reject for command line MAP trees. ")
 	public String burnin = "0";
+	
+	/** Estimated Sample Size Test only */
+	@Parameter(names = {"-m","--maxaposterioritree"}, description = "Calculate and display MAP tree. ")
+	public boolean maptree = false;
+	
+	/** Estimated Sample Size Test only */
+	@Parameter(names = {"-ct","--convergencetest"}, description = "Run the overall convergence tests only. ")
+	public boolean convergencetest = false;
 }
