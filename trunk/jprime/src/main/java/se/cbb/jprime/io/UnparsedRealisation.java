4ce8e1a1b22d6942c2ef2ac92df111c4430cf81c
owais.mahmudi@gmail.com <owais.mahmudi@gmail.com@04b1e456-affd-6f09-912b-b9d4e44ba6e1>
2014-01-20 23:41:18 +0000

    After implementing heatmap generation

--- trunk/jprime/src/main/java/se/cbb/jprime/io/UnparsedRealisation.java
+++ trunk/jprime/src/main/java/se/cbb/jprime/io/UnparsedRealisation.java
@@ -3,6 +3,15 @@ package se.cbb.jprime.io;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import se.cbb.jprime.apps.dltrs.Realisation;
+import se.cbb.jprime.topology.BooleanMap;
+import se.cbb.jprime.topology.NamesMap;
+import se.cbb.jprime.topology.RBTree;
+import se.cbb.jprime.topology.RootedBifurcatingTree;
+import se.cbb.jprime.topology.StringMap;
+import se.cbb.jprime.topology.TimesMap;
+import se.cbb.jprime.topology.TopologyException;
+
 /**
  * Holds an unparsed rooted tree realisation as a PrIME Newick tree. All the properties of the realisation can be accessed through the
  * PrIME Newick tree properties. Some useful methods are provided, however, such as string representations for e.g. hashing or comparisons.
@@ -60,6 +69,109 @@ public class UnparsedRealisation {
 		this.subSampleID = subSampleID;
 	}
 	
+	/**
+	 * Parse a string of realization (DLTRS) to the realization object
+	 * @throws NewickIOException 
+	 * @throws TopologyException 
+	 */
+	public static Realisation parseRealisation(String real) throws NewickIOException, TopologyException
+	{
+		PrIMENewickTree tree = PrIMENewickTreeReader.readTree(real, true, true);
+		TimesMap times = tree.getTimesMap(real);	
+		String[] names = new String[tree.getNoOfVertices()];
+		boolean[] isdups = new boolean[tree.getNoOfVertices()];
+		boolean[] istrans = new boolean[tree.getNoOfVertices()];
+		String[] placements = new String[tree.getNoOfVertices()];
+		String[] fromTos = new String[tree.getNoOfVertices()];
+		
+		for(int v =0; v < tree.getNoOfVertices(); v++)
+		{
+			names[v]=tree.getVertex(v).getName();
+			String meta = tree.getVertex(v).getMeta();
+			// 0 for leaf, 1 for speciation, 2 for duplication, 3 for transfer
+			int vertextype = getVertexType(meta);
+			
+			isdups[v] = false;
+			istrans[v] = false;
+			
+			if (vertextype == 2)
+				isdups[v]=true;
+			else if( vertextype == 3)
+				istrans[v]=true;
+			
+			
+			int [] fromtos = {-1, -1, -1};	
+			if (vertextype == 3)
+				fromtos = getFromToPoints(meta);
+			fromTos[v] = "("+fromtos[0]+","+fromtos[1]+","+fromtos[2]+")";
+			
+			
+			int[] placement = getRealisedPoint(meta);
+			placements[v]= "("+placement[0]+","+placement[1]+")";
+		}
+		
+		
+		NamesMap Names = new NamesMap("GuestTreeNames", names);
+		BooleanMap isDups = new BooleanMap("RealisationIsDups", isdups);
+		BooleanMap isTrans = new BooleanMap("RealisationIsTrans", istrans);
+		StringMap Placements = new StringMap("DiscPts",placements);
+		StringMap FromTos = new StringMap("fromToLineage",fromTos);
+
+		RBTree rbtree = new RBTree((NewickTree) tree,"");
+		Realisation realisation = new Realisation((RootedBifurcatingTree) rbtree, Names, times, isDups, isTrans, Placements, FromTos);
+		return (realisation );
+	}
+	
+	
+	/**
+	 * Returns the vertex type of the meta
+	 * 0 for leaf, 1 for speciation, 2 for duplication, 3 for transfer
+	 * @param meta represents the realized information of vertex
+	 * @return vertex type
+	 */
+	private static int getVertexType(String meta){
+		String str = meta.substring(meta.indexOf("VERTEXTYPE=")+11 , meta.indexOf("VERTEXTYPE=")+15 );
+		if (str.equals("Leaf"))
+			return 0;
+		else if (str.equals("Spec"))
+			return 1;
+		else if (str.equals("Dupl"))
+			return 2;
+		else if (str.equals("Tran"))
+			return 3;
+		else 
+			return -1;
+
+	}
+	
+	/**
+	 * Returns the FromTo transfer points of the meta
+	 * @param meta represents the realized information of vertex
+	 * @return y [realised points in array]
+	 */
+	private static int[] getFromToPoints(String meta){
+		String str = meta.substring(meta.indexOf("FROMTOLINEAGE=("), meta.indexOf("FROMTOLINEAGE=(")+23);
+		
+		int y1 = Integer.parseInt(str.substring(str.indexOf("FROMTOLINEAGE=(")+15 , str.indexOf(",") ));
+		int y2 = Integer.parseInt(str.substring(str.indexOf(",")+1 , str.lastIndexOf(",") ));
+		int y3 = Integer.parseInt(str.substring(str.lastIndexOf(",")+1 , str.lastIndexOf(")") ));
+
+		int y[] = {y1, y2, y3};
+		return y;
+	}	
+	
+	/**
+	 * Returns the Realization points of the meta
+	 * @param meta represents the realized information of vertex
+	 * @return y [realised points in array]
+	 */
+	private static int[] getRealisedPoint(String meta){
+		
+		int y1 = Integer.parseInt(meta.substring(meta.indexOf("DISCPT=(")+8 , meta.lastIndexOf(",") ));
+		int y2 = Integer.parseInt(meta.substring(meta.lastIndexOf(",")+1 , meta.lastIndexOf(")") ));
+		int y[] = {y1, y2};
+		return y;
+	}
 	/**
 	 * For string comparisons, hashing, etc., returns a sorted Newick tree representation where a user-defined
 	 * level of info has been stripped away.
